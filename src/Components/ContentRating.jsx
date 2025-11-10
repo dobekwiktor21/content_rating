1c53e08a802174fb9a3c10d96f7ebc73c0400ad5
dobekwiktor21 <dobekwikorbusiness01@gmail.com>
2025-11-10 17:43:12 +0100

    App done but we have but in preview

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,44 @@
 
-import React, { Component } from 'react';
-import './ContentRating.css';
+     import React, { Component } from 'react';
+     import './ContentRating.css';
 
-class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
-}
+     class ContentRating extends Component {
+       constructor() {
+         super();
+         this.state = {
+             likes: 0,
+             dislikes: 0,
+           handleLike:() => {
+             this.setState((prevState) => ({
+                 likes: prevState.likes + 1
+               }));
+           },
+           handleDislike:() => {
+             this.setState((prevState) => ({
+                 dislikes: prevState.dislikes + 1
+               }));
+           }
+           };
+       }
+       render() {
+         return (
+          <>
+          <h1>Ocena treści tekstu</h1>
+          <div className='content-rating'>
+             <p>Tekst</p>
+             <div className='rating-buttons'>
+             <button className="like-button" onClick={this.state.handleLike}>
+                 Lubię to ({this.state.likes})
+               </button>
+               <button className="dislike-button" onClick={this.state.handleDislike}>
+                 Nie lubię ({this.state.dislikes})
+               </button>
+             </div>
 
-export default ContentRating;
+          </div>
+          </>
+         );
+       }
+     }
+
+     export default ContentRating;
